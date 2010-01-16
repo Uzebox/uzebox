cd4f29acb15ef1dc805f11aecaf436a54d9a8e28
uze@belogic.com <uze@belogic.com@c007265c-c25a-11dd-ae62-6dab0a2b1684>
2010-01-16 17:44:59 +0000

    Deleted uzebox-3.0-stable branch. Not required with new guidelines

--- branches/uzebox-3.0-stable/tools/UzeboxTools/dist/midiconv.bat
+++
@@ -1,4 +0,0 @@
-@REM Insure java is in your system PATH
-@REM %~dp0 is where the batch lives, not sure how to do that in UNIX... 
-
-java -cp %~dp0/uzetools.jar com.belogic.uzebox.tools.converters.midi.MidiConvert %*
