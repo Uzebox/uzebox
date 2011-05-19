c45255e19adb5dea069d57cd4fc7406135162b05
uze@belogic.com <uze@belogic.com@c007265c-c25a-11dd-ae62-6dab0a2b1684>
2011-05-19 19:00:45 +0000

    Added GPL text.

--- demos/Bootloader/bootloader.c
+++ demos/Bootloader/bootloader.c
@@ -1,3 +1,33 @@
+/*
+ *  Uzebox(tm) Gameloader
+ *  Copyright (C) Alec Bourque
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Uzebox is a reserved trade mark
+*/
+
+/*
+About this program:
+-------------------
+This program is an AVR bootloader that allows to read and flash games from a SD/MMC card. 
+The bootloader requires 4K of flash.
+
+The Atmega644 needs to have some fuses set in order to support teh bootloader. For setup details see: http://uzebox.org/forums/viewtopic.php?p=3847#p3847
+
+*/
+
 #include <stdbool.h>
 #include <avr/io.h>
 #include <stdlib.h>
