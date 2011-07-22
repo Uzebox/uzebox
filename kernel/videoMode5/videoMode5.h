0e520a1dd241851d858d7f5a3e5fe5f248084ea9
uze@belogic.com <uze@belogic.com@c007265c-c25a-11dd-ae62-6dab0a2b1684>
2011-07-22 04:56:42 +0000

    -Added video mode 5 sources

---
+++ kernel/videoMode5/videoMode5.h
@@ -0,0 +1,32 @@
+/*
+ *  Uzebox(tm) Video Mode 1
+ *  Copyright (C) 20098  Alec Bourque
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Uzebox is a reserved trade mark
+*/
+
+/** 
+ * ==============================================================================
+ *
+ * This file contains function prototypes and exports for mode 1
+ *
+ * ===============================================================================
+ */
+ #pragma once
+	
+	extern unsigned char vram[];  
+		
+
