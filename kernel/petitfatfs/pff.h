3245033a339a10a62b750e96ed2a5b4b0760925d
uze@belogic.com <uze@belogic.com@c007265c-c25a-11dd-ae62-6dab0a2b1684>
2013-06-29 01:02:26 +0000

    -Added PetitFatFs library

--- kernel/petitfatfs/pff.h
+++ kernel/petitfatfs/pff.h
@@ -26,13 +26,14 @@
 #ifndef _FATFS
 #define _FATFS
 
+
 #define	_USE_READ	1	/* 1:Enable pf_read() */
 
 #define	_USE_DIR	1	/* 1:Enable pf_opendir() and pf_readdir() */
 
 #define	_USE_LSEEK	1	/* 1:Enable pf_lseek() */
 
-#define	_USE_WRITE	0	/* 1:Enable pf_write() */
+#define	_USE_WRITE	1	/* 1:Enable pf_write() */
 
 #define _FS_FAT12	1	/* 1:Enable FAT12 support */
 #define _FS_FAT32	1	/* 1:Enable FAT32 support */
