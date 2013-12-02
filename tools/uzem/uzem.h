7ceb704b6a6cae4fec6670def186878074be8459
uze@belogic.com <uze@belogic.com@c007265c-c25a-11dd-ae62-6dab0a2b1684>
2013-12-02 04:22:34 +0000

    -Upgraded version to 1.18

--- tools/uzem/uzem.h
+++ tools/uzem/uzem.h
@@ -4,7 +4,7 @@ PLEASE SEE THE FORUM FOR MORE DETAILS:  http://uzebox.org/forums/
 
 (The MIT License)
 
-Copyright (c) 2008,2009, David Etherton, Eric Anderton
+Copyright (c) 2008-2013, David Etherton, Eric Anderton, Alec Bourque
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -30,6 +30,6 @@ THE SOFTWARE.
 // force application for GUI mode
 #define GUI 1 
 
-#define VERSION "v1.17"
+#define VERSION "v1.18"
 
 #endif
