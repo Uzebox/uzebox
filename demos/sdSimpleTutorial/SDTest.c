6ea04fd2276f99087f7470e7ae12d5c5d7872942
CunningFellow <andrewm1973@users.noreply.github.com>
2015-10-31 18:20:22 +1000

    changed names of sdSimple to sdBase

--- demos/sdSimpleTutorial/SDTest.c
+++ demos/sdSimpleTutorial/SDTest.c
@@ -24,7 +24,7 @@
 #include <uzebox.h>
 #include "data/tiles.inc"
 #include "data/fonts6x8.inc"
-#include <sdSimple.h>
+#include <sdBase.h>
 
 
 #define COORD(X, Y) (X + (Y*40))
@@ -41,7 +41,8 @@ ClearVram();					//Clear the screen (fills the vram with tile zero)
 long sectorStart;
 
 sdCardInitNoBuffer();
-sectorStart = sdFindFileFirstSectorFlash(fileName);
+
+sectorStart = sdCardFindFileFirstSectorFlash(fileName);
 
 if(sectorStart == 0){
 	Print(0,0,PSTR("FILE HELLWRLD.TXT NOT FOUND ON SD CARD"));
@@ -60,16 +61,16 @@ if(sectorStart == 0){
 
 	*/
 
-    // sdDirectReadSimple(uint8_t *dest, uint16_t count);
+    // sdCardDirectReadSimple(uint8_t *dest, uint16_t count);
     // sdDirectRead(uint8_t *dest, uint16_t count, uint8_t span, uint8_t run);
     //
     //
     // HELLO WORLD FROM THE SD CARD
     // Read 28 bytes of test straight to VRAM
-    // sdDirectReadSimple(address, length) == sdDriectReadSimple(address, length, 0, 0)
+    // sdCardDirectReadSimple(address, length) == sdDriectReadSimple(address, length, 0, 0)
     // That is SPAN = 0 RUN = 0
 
-    sdDirectReadSimple(&vram[COORD(0,0)], 28);    // HELLO WORLD
+    sdCardDirectReadSimple(&vram[COORD(0,0)], 28);    // HELLO WORLD
     sdCardSkipBytes(1);                           // Skip the CR/LF
 
     // VERTICAL TEXT
@@ -80,7 +81,7 @@ if(sectorStart == 0){
     //
     // Screen width = 40 so 39 is screen width - 1
 
-    sdDirectRead(&vram[COORD(0,2)], 13, 39, 1);   // VERTICAL TEXT
+    sdCardDirectRead(&vram[COORD(0,2)], 13, 39, 1);   // VERTICAL TEXT
     sdCardSkipBytes(1);                           // Skip the CR/LF
 
     // DOUBLE SPACES
@@ -89,7 +90,7 @@ if(sectorStart == 0){
     //
     // Every 1 (RUN) byte skip 1 (SPAN) bytes
 
-    sdDirectRead(&vram[COORD(3,2)], 13, 1, 1);    // DOUBLE SPACES
+    sdCardDirectRead(&vram[COORD(3,2)], 13, 1, 1);    // DOUBLE SPACES
     sdCardSkipBytes(1);                           // Skip the CR/LF
 
     // DOUBLE SPACES
@@ -100,7 +101,7 @@ if(sectorStart == 0){
     // 40 is one whole line of text
     // 39 is (40-1)
 
-    sdDirectRead(&vram[COORD(0,17)], 80, 39, 40); // DOUBLE LINE SPACING
+    sdCardDirectRead(&vram[COORD(0,17)], 80, 39, 40); // DOUBLE LINE SPACING
     sdCardSkipBytes(1);                           // Skip the CR/LF
 
     // 16 bit modes
@@ -111,9 +112,29 @@ if(sectorStart == 0){
     // 2 bytes = 16 bits.  So modes with a 16 bit tile index could use this to fill in a column
     // 38 is (40-2)
 
-    sdDirectRead(&vram[COORD(20,4)], 22, 38, 2);  // 16 BIT MODES
+    sdCardDirectRead(&vram[COORD(20,4)], 22, 38, 2);  // 16 BIT MODES
 }
 
 while(1);
 
 }
+
+
+
+
+/*
+Print(0,1,PSTR("     "));
+
+PrintHexInt(0,0, (SD_DEBUG_bootRecordSector>>16));PrintHexInt(4,0, SD_DEBUG_bootRecordSector); Print(9,0,PSTR("BOOT RECORD SECTOR"));
+PrintHexInt(4,1, SD_DEBUG_bytesPerSector); Print(9,1,PSTR("BYTES PER SECTOR"));
+PrintHexByte(6,2, SD_DEBUG_sectorsPerCluster); Print(9,2,PSTR("SECTORS PER CLUSTER"));
+PrintHexInt(4,3, SD_DEBUG_reservedSectors); Print(9,3,PSTR("RESERVED SECTORS"));
+PrintHexInt(4,4, SD_DEBUG_maxRootDirectoryEntries); Print(9,4,PSTR("MAX ROOT DIR ENTRY"));
+PrintHexInt(4,5, SD_DEBUG_sectorsPerFat); Print(9,5,PSTR("SECTROS PER FAT"));
+PrintHexInt(0,6, (SD_DEBUG_dirTableSector>>16)); PrintHexInt(4,6, SD_DEBUG_dirTableSector); Print(9,6,PSTR("DIR TABLE SECTOR"));
+PrintHexInt(4,7, SD_DEBUG_firstCluster); Print(9,7,PSTR("FIRST CLUSTER"));
+PrintHexInt(0,8, (SD_DEBUG_firstSector>>16));PrintHexInt(4,8, SD_DEBUG_firstSector); Print(9,8,PSTR("FIRST SECTOR"));
+
+while(1);
+
+*/
