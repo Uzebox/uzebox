4b56f543df79ceae6a02477a3e4604b9c264bd09
CunningFellow <andrewm1973@users.noreply.github.com>
2015-11-01 11:07:25 +1000

    removed tornado2000 from master branch

--- demos/tornado2000/default/go.bat
+++
@@ -1,4 +0,0 @@
-del Test\t2k.hex
-copy t2k.hex Test\
-cd Test
-start t2k.hex
--- demos/tornado2000/housekeeping.c
+++
@@ -1,8 +0,0 @@
-/*
- * housekeeping.c
- *
- *  Created on: 17/12/2014
- *      Author: Cunning Fellow
- */
-#include <avr/io.h>
-
