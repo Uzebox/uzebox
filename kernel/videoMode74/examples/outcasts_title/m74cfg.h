ddeae1485e64a55a44ff6eb9256d3b8f9b0bd229
Sandor Zsuga <jubatian@jubatian.com>
2015-12-16 20:37:00 +0100

    MODE74: Two examples added to demonstrate some of the video mode.
    One is the Outcasts title screen concept which demonstrates Row mode 3 (2bpp
    Multicolor), and some palette effects.
    
    The other is the dragon disc sprite demo, showing off most of the sprite
    engine's features over a simple tiled background (Row mode 0).

---
+++ kernel/videoMode74/examples/outcasts_title/m74cfg.h
@@ -0,0 +1,42 @@
+/*
+ *  Mode 74 configuration
+ *  Copyright (C) 2015 Sandor Zsuga (Jubatian)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef M74CFG_H
+#define M74CFG_H
+
+
+/* Where the tile & similar aligned data is placed (not Mode 74 specific) */
+#define TILES_SECT 0x4A00
+
+/* Mode 74 specifics (Note: TBANKM0, TBANK2 and TBANKM3 are not filled, just
+** added since required by Mode 74. The entire title screen uses
+** Multicolor) */
+
+#define M74_TBANKM0_0_OFF  0
+#define M74_TBANK2_0_OFF   0
+#define M74_TBANK3_0_OFF   0
+#define M74_TBANK3_0_INC   0
+
+#define M74_TBANK01_0_OFF  (TILES_SECT + 0x0000)
+#define M74_TBANK01_0_INC  0
+#define M74_M3_ENABLE      1
+#define M74_M3_OFF         (0x0F00-(9*39)-2304)
+
+
+#endif
