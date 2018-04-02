cd1a2c7ee27309d398d67ff56436f80a9cf11180
Sandor Zsuga <jubatian@jubatian.com>
2018-04-02 11:18:18 +0200

    MODE748: New example: Background scrolling
    Demonstrates a scrolling SPI RAM background with sprites.

---
+++ kernel/videoMode748/examples/sprite_scroll/tiles.h
@@ -0,0 +1,34 @@
+/*
+ *  Mode 748 Sprite demo resources
+ *  Copyright (C) 2015 - 2018 Sandor Zsuga (Jubatian)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+
+
+#ifndef TILES_H
+#define TILES_H
+
+
+extern const unsigned char imgvram[];
+extern const unsigned char res_tiles_00[];
+extern const unsigned char res_pal_00[];
+extern const unsigned char res_sprites_00[];
+
+
+#endif
